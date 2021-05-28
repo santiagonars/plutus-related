2aa24b21f3e46c7bb690a44fabec61b283881d9e
Santiago Norena <san.norena@gmail.com>
2021-05-27 22:00:07 -0400

    Create syntax.hs

---
+++ haskell/syntax.hs
@@ -0,0 +1,30 @@
+-- Guards and where example; Alternative use=> (skinny, normal, fat) = (18.5, 25.0, 30.0) 
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          skinny = 18.5  
+          normal = 25.0  
+          fat = 30.0    
+
+-- Custom max function
+max' :: (Ord a) => a -> a -> a  
+max' a b   
+    | a > b     = a  
+    | otherwise = b  
+
+-- Custom compare function
+myCompare :: (Ord a) => a -> a -> Ordering  
+a `myCompare` b  
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT  
+
+-- Get a first and a last name and give someone back their initials
+initials :: String -> String -> String  
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."  
+    where (f:_) = firstname  
+          (l:_) = lastname 
