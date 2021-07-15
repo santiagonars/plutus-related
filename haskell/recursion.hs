83e5264b8392172c0df8a3136f3a974f25fb7c1a
Santiago Norena <san.norena@gmail.com>
2021-07-15 01:55:20 -0400

    Update recursion.hs

---
+++ haskell/recursion.hs
@@ -0,0 +1,50 @@
+-- example for custom implementation of a maximum function
+maximum' :: (Ord a) => [a] -> a  
+maximum' [] = error "maximum of empty list"  
+maximum' [x] = x  
+maximum' (x:xs)   
+    | x > maxTail = x  
+    | otherwise = maxTail  
+    where maxTail = maximum' xs  
+-- alternative approach using built-in max function
+maximum'' :: (Ord a) => [a] -> a  
+maximum'' [] = error "maximum of empty list"  
+maximum'' [x] = x  
+maximum'' (x:xs) = max x (maximum' xs) 
+
+-- Custom function for replicate
+replicate' :: (Num i, Ord i) => i -> a -> [a]  
+replicate' n x  
+    | n <= 0    = []  
+    | otherwise = x:replicate' (n-1) x 
+
+-- Custom take function
+take' :: (Num i, Ord i) => i -> [a] -> [a]  
+take' n _  
+    | n <= 0   = []  
+take' _ []     = []  
+take' n (x:xs) = x : take' (n-1) xs 
+
+-- Custom reverse function
+reverse' :: [a] -> [a]  
+reverse' [] = []  
+reverse' (x:xs) = reverse' xs ++ [x]
+
+-- Custom repeat function (creates an infinite list)
+repeat' :: a -> [a]  
+repeat' x = x:repeat' x 
+
+-- Custom elem function
+elem' :: (Eq a) => a -> [a] -> Bool  
+elem' a [] = False  
+elem' a (x:xs)  
+    | a == x    = True  
+    | otherwise = a `elem'` xs
+
+-- Quick sort algorithm !!!
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted 
