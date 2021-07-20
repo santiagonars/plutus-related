3aeb06f4cb9774a3762c2a52979f7b65add8df54
Santiago Norena <san.norena@gmail.com>
2021-07-19 21:00:05 -0400

    Improved code for MkValidator

--- plutus-pioneer-program/week03/src/Week03/Homework1.hs
+++ plutus-pioneer-program/week03/src/Week03/Homework1.hs
@@ -46,18 +46,13 @@ PlutusTx.unstableMakeIsData ''VestingDatum
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkValidator :: VestingDatum -> () -> ScriptContext -> Bool
-mkValidator dat () ctx = signedByCorrectBeneficiary
+mkValidator dat () ctx
+    | (txSignedBy info $ beneficiary1 dat) && (contains (to $ deadline dat) $ txInfoValidRange info) = True
+    | (txSignedBy info $ beneficiary2 dat) && (contains (from $ deadline dat) $ txInfoValidRange info) = True
+    | otherwise = traceError "Either dealine has passed for beneficiary 1, it has not been met for beneficiary 2, or invalid beneficiary"
   where
     info :: TxInfo
     info = scriptContextTxInfo ctx
-
-    signedByBeneficiary :: Bool
-    signedByCorrectBeneficiary
-        | (txSignedBy info $ beneficiary1 dat) && (contains (to $ deadline dat) $ txInfoValidRange info) = True
-        | (txSignedBy info $ beneficiary2 dat) && (contains (from $ deadline dat) $ txInfoValidRange info) = True
-        | otherwise = traceError "Either dealine has passed for beneficiary 1, it has not been met for beneficiary 2, or invalid beneficiary"
-
-
 
 data Vesting
 instance Scripts.ValidatorTypes Vesting where
