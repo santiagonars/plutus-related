8d3dc6b5fdec7c861db3f4e50b1f497b5deaea6e
Santiago Norena <san.norena@gmail.com>
2021-07-26 14:35:11 -0400

    Update Writer.hs

--- plutus-pioneer-program/week04/src/Week04/Writer.hs
+++ plutus-pioneer-program/week04/src/Week04/Writer.hs
@@ -2,31 +2,34 @@ module Week04.Writer where
 
 import Control.Monad
 import Week04.Monad
-
-data Writer a = Writer a [String]
+-- Thesis: The idea of creating  computations that can also produce lock output
+data Writer a = Writer a [String] -- constructor takes 2 arguments; Writer a & a list of lock messages
     deriving Show
 
 number :: Int -> Writer Int
-number n = Writer n $ ["number: " ++ show n]
+number n = Writer n $ ["number: " ++ show n] -- given n, produces that number and as log output
 
-tell :: [String] -> Writer ()
+tell :: [String] -> Writer () -- gets a list of lock messages and produces a Writer unit with the log messages
 tell = Writer ()
 
-foo :: Writer Int -> Writer Int -> Writer Int -> Writer Int
+-- combine 3 computations that add the 3 Int and concatenate the log message of each one to get an overall message
+foo :: Writer Int -> Writer Int -> Writer Int -> Writer Int 
 foo (Writer k xs) (Writer l ys) (Writer m zs) =
   let
     s = k + l + m
-    Writer _ us = tell ["sum: " ++ show s]
+    Writer _ us = tell ["sum: " ++ show s] -- Write () us
   in
     Writer s $ xs ++ ys ++ zs ++ us
 
+-- given a Writer that produces an a and a function from a to Writer b, are combine to produce a Writer b
 bindWriter :: Writer a -> (a -> Writer b) -> Writer b
 bindWriter (Writer a xs) f =
   let
     Writer b ys = f a
   in
-    Writer b $ xs ++ ys
+    Writer b $ xs ++ ys -- binds log messages from a and b
 
+-- The bindWriter in foo' has a logic of sequencing log outputs producing computations by combining the log outputs
 foo' :: Writer Int -> Writer Int -> Writer Int -> Writer Int
 foo' x y z = x `bindWriter` \k ->
              y `bindWriter` \l ->
