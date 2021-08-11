e4c258c33c5254877fc7bde8f0a896f240483119
Santiago Norena <san.norena@gmail.com>
2021-08-10 23:29:08 -0400

    Update Funds.hs

--- plutus-pioneer-program/week06/src/Week06/Oracle/Funds.hs
+++ plutus-pioneer-program/week06/src/Week06/Oracle/Funds.hs
@@ -10,7 +10,7 @@
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
-
+-- This module provides 2 contracts
 module Week06.Oracle.Funds
     ( ownFunds
     , ownFunds'
@@ -25,17 +25,18 @@ import           PlutusTx.Prelude hiding ((<$>))
 import           Prelude          (Show (..), String, (<$>))
 import           Ledger           hiding (singleton)
 import           Ledger.Value     as Value
-
+-- OwnFunds function sum up all value in own wallet's UTXOs 
 ownFunds :: Contract w s Text Value
 ownFunds = do
-    pk    <- ownPubKey
-    utxos <- utxoAt $ pubKeyAddress pk
-    let v = mconcat $ Map.elems $ txOutValue . txOutTxOut <$> utxos
-    logInfo @String $ "own funds: " ++ show (Value.flattenValue v)
-    return v
+    pk    <- ownPubKey -- get own public key
+    utxos <- utxoAt $ pubKeyAddress pk -- get a map of UTXOs (references) sitting in own wallet; look up UTXOs at the address given by public key
+    let v = mconcat $ Map.elems $ txOutValue . txOutTxOut <$> utxos -- first get the output belonging to a UTXO, and then the value. This become a map from UTXO references to values
+    logInfo @String $ "own funds: " ++ show (Value.flattenValue v)  -- Map.elems ignores the keys and gives a list of all values; mconcat combines the list the same type (adds up all the values)
+    return v  -- v is the sum of all the values of all UTXOs in own wallet
 
+-- A variation of ownFunds; Instead of returning the value, it permanantly tells it
 ownFunds' :: Contract (Last Value) Empty Text ()
 ownFunds' = do
-    handleError logError $ ownFunds >>= tell . Last . Just
-    void $ Contract.waitNSlots 1
-    ownFunds'
+    handleError logError $ ownFunds >>= tell . Last . Just -- use monadic bind (>>=) to tells it; so it write the sum of funds owned to the log
+    void $ Contract.waitNSlots 1 -- wait 1 slot
+    ownFunds' -- call recursively; it will run forever and every slot check own funds available and write it into the log
