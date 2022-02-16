cbb71b7686f59e5ccf6bcdbf206f99332c43b4ae
Santiago Norena <san.norena@gmail.com>
2022-02-15 21:12:06 -0500

    Update higher_order_functions.hs

--- haskell/higher_order_functions.hs
+++ haskell/higher_order_functions.hs
@@ -5,13 +5,30 @@ compareWithHundred x = compare 100 x
 compareWithHundred' :: (Num a, Ord a) => a -> Ordering  
 compareWithHundred' = compare 100 
 
+-- use sections to partially apply infix functions
+divideByTen :: (Floating a) => a -> a
+divideByTen  = (/10)
+
 -- check if a character supplied to it is an uppercase letter as a partial function
 isUpperAlphanum :: Char -> Bool  
 isUpperAlphanum = (`elem` ['A'..'Z']) 
 
--- ONLY EXCEPTION: Instead of doing (-4)
+-- ONLY EXCEPTION: The negative (-) sign; do not use something like (-4)
 subFour :: Integer -> Integer 
 subFour = (subtract 4)  
 
-app :: (a -> b) -> a -> b -- use running this example which return 2:   app (\x -> x+1) 1
+-- use running this example which return 2:   app (\x -> x+1) 1
+-- example takes a function (which takes x and returns x+1) 
+--               and x (which is used in the supplied function)
+app :: (a -> b) -> a -> b
 app f x = f x
+
+-- Functions can take functions as parameters and also return functions
+applyTwice :: (a -> a) -> a -> a  
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]  
+zipWith' _ [] _ = []  
+zipWith' _ _ [] = []  
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
