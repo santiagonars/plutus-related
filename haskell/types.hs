765b66a9e9a5972e774ead9b3684eeef396ffbe9
Santiago Norena <san.norena@gmail.com>
2022-08-25 20:17:59 -0400

    Create types.hs

---
+++ haskell/types.hs
@@ -0,0 +1,55 @@
+import qualified Data.Map as Map
+
+data Person = Person String String Int Float String String deriving (Show)
+
+firstName :: Person -> String  
+firstName (Person firstname _ _ _ _ _) = firstname  
+  
+lastName :: Person -> String  
+lastName (Person _ lastname _ _ _ _) = lastname  
+  
+age :: Person -> Int  
+age (Person _ _ age _ _ _) = age  
+  
+height :: Person -> Float  
+height (Person _ _ _ height _ _) = height  
+  
+phoneNumber :: Person -> String  
+phoneNumber (Person _ _ _ _ number _) = number  
+  
+flavor :: Person -> String  
+flavor (Person _ _ _ _ _ flavor) = flavor
+
+-- A better way to write Person is by using curly brackets (Record Syntax)!!!!
+
+
+-- Below is an example of using Either a b
+-- map from Data.Map to represent the lockers. 
+-- It'll map from locker numbers to a pair of whether the locker is in use or not and the locker code.
+--uses import qualified Data.Map as Map  
+  
+data LockerState = Taken | Free deriving (Show, Eq)  
+  
+type Code = String  
+  
+type LockerMap = Map.Map Int (LockerState, Code)  
+
+lockerLookup :: Int -> LockerMap -> Either String Code  
+lockerLookup lockerNumber map =   
+    case Map.lookup lockerNumber map of   
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"  
+        Just (state, code) -> if state /= Taken   
+                                then Right code  
+                                else Left $ "Locker " ++ show lockerNumber ++ " is already taken!" 
+
+lockers :: LockerMap  
+lockers = Map.fromList   
+    [(100,(Taken,"ZD39I"))  
+    ,(101,(Free,"JAH3I"))  
+    ,(103,(Free,"IQSA9"))  
+    ,(105,(Free,"QOTSA"))  
+    ,(109,(Taken,"893JJ"))  
+    ,(110,(Taken,"99292"))  
+    ]  
+
+
